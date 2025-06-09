edcc4591c46f23d772d8222de197f10b0b121490
TecnologiaJNG <142359035+Tecnologia-JNG@users.noreply.github.com>
2025-06-09 10:47:05 -0300

    Create profissao.js

---
+++ profissao.js
@@ -0,0 +1,565 @@
+const clienteProfissao = "<?= $clienteProfissao ?>";
+
+const profissoes = [{
+dsProfissao: "ACOUGUEIRO"
+}, {
+dsProfissao: "ACUPUNTURISTA"
+}, {
+dsProfissao: "ADMINISTRADOR"
+}, {
+dsProfissao: "ADMINISTRADOR DE COMARCA"
+}, {
+dsProfissao: "ADMINISTRADOR DE TRIBUNAL"
+}, {
+dsProfissao: "ADVOGADO"
+}, {
+dsProfissao: "AERONAUTA"
+}, {
+dsProfissao: "AEROVIARIO"
+}, {
+dsProfissao: "AGENCIADOR DE PROPAGANDA"
+}, {
+dsProfissao: "AGENTE ADMINISTRATIVO"
+}, {
+dsProfissao: "AGENTE COMUNITARIO DE SAUDE"
+}, {
+dsProfissao: "AGENTE DE SERVICOS FUNERARIOS E EMBALSAMADOR"
+}, {
+dsProfissao: "AGENTE DE VIAGEM E GUIA DE TURISMO"
+}, {
+dsProfissao: "AGRICULTOR"
+}, {
+dsProfissao: "AGRIMENSOR"
+}, {
+dsProfissao: "AGRONOMO"
+}, {
+dsProfissao: "AGROPECUARISTA"
+}, {
+dsProfissao: "ALFAIATE"
+}, {
+dsProfissao: "AMBULANTE"
+}, {
+dsProfissao: "ANALISTA"
+}, {
+dsProfissao: "ANALISTA DE SISTEMA"
+}, {
+dsProfissao: "ANTIQUARIO"
+}, {
+dsProfissao: "ANTROPOLOGO"
+}, {
+dsProfissao: "APICULTOR"
+}, {
+dsProfissao: "APOSENTADO / PENSIONISTA"
+}, {
+dsProfissao: "APRENDIZ BANCO DO BRASIL"
+}, {
+dsProfissao: "AQUICULTOR"
+}, {
+dsProfissao: "ARMEIRO"
+}, {
+dsProfissao: "ARQUEOLOGO E MUSEOLOGO"
+}, {
+dsProfissao: "ARQUITETO OU PAISAGISTA"
+}, {
+dsProfissao: "ARTESAO"
+}, {
+dsProfissao: "ARTISTA PLASTICO"
+}, {
+dsProfissao: "ASSISTENTE SOCIAL"
+}, {
+dsProfissao: "ASTROLOGO, NUMEROLOGO E AFINS"
+}, {
+    dsProfissao: "ASTRONOMO"
+}, {
+dsProfissao: "ATLETA PROFISSIONAL E TECNICO EM DESPORTOS"
+}, {
+dsProfissao: "ATOR E DIRETOR DE ESPETACULOS PUBLICOS"
+}, {
+dsProfissao: "AUXILIAR DE ENFERMAGEM"
+}, {
+dsProfissao: "AUXILIAR DE ESCRITORIO E ASSEMELHADOS"
+}, {
+dsProfissao: "AUXILIAR DE LABORATORIO E RAIOS X"
+}, {
+dsProfissao: "AUXILIAR DE NUTRICAO"
+}, {
+dsProfissao: "AUXILIAR DE SERVICOS GERAIS"
+}, {
+dsProfissao: "AVICULTOR"
+}, {
+dsProfissao: "BANCARIO E ECONOMIARIO"
+}, {
+dsProfissao: "BARBEIRO, CABELEIREIRO, MANICURE E PEDICURE"
+}, {
+dsProfissao: "BIBLIOTECARIO OU ARQUIVISTA"
+}, {
+dsProfissao: "BIOFISICO"
+}, {
+dsProfissao: "BIOLOGO OU BIOMEDICO"
+}, {
+dsProfissao: "BIOQUIMICO"
+}, {
+dsProfissao: "BOLSISTA MESTRADO/DOUTORADO"
+}, {
+dsProfissao: "BOLSISTA OUTROS"
+}, {
+dsProfissao: "BOMBEIRO E INSTALADOR DE GAS, AGUA, ESGOTO E ASSEMELHADOS"
+}, {
+dsProfissao: "BORRACHEIRO"
+}, {
+dsProfissao: "CANTOR E COMPOSITOR"
+}, {
+dsProfissao: "CAPITALISTA"
+}, {
+dsProfissao: "CAPITALISTA/RENTISTA/LOCADOR"
+}, {
+dsProfissao: "CARTEIRO"
+}, {
+dsProfissao: "CENOGRAFO"
+}, {
+dsProfissao: "CERAMISTA"
+}, {
+dsProfissao: "CHAVEIRO"
+}, {
+dsProfissao: "COBRADOR DE TRANSPORTE COLETIVO"
+}, {
+dsProfissao: "COMERCIARIO"
+}, {
+dsProfissao: "COMUNICOLOGO"
+}, {
+dsProfissao: "CONSELHEIRO TUTELAR"
+}, {
+dsProfissao: "CONSULTOR LEGISLATIVO"
+}, {
+dsProfissao: "CONTADOR"
+}, {
+dsProfissao: "COREOGRAFO E BAILARINO"
+}, {
+dsProfissao: "CORREGEDOR"
+}, {
+dsProfissao: "CORRETOR"
+}, {
+dsProfissao: "COSTUREIRO"
+}, {
+dsProfissao: "COZINHEIRO, COPEIRO, GARCOM, BARMEN E CAMAREIRO"
+}, {
+dsProfissao: "DECORADOR"
+}, {
+dsProfissao: "DEFENSOR PUBLICO"
+}, {
+dsProfissao: "DELEGADO DE POLICIA"
+}, {
+dsProfissao: "DEPUTADO ESTADUAL/DISTRITAL"
+}, {
+dsProfissao: "DEPUTADO FEDERAL"
+}, {
+dsProfissao: "DESEMBARGADOR"
+}, {
+dsProfissao: "DESENHISTA"
+}, {
+dsProfissao: "DESENHISTA COMERCIAL"
+}, {
+dsProfissao: "DESENHISTA INDUSTRIAL"
+}, {
+dsProfissao: "DESENHISTA TECNICO"
+}, {
+dsProfissao: "DESPACHANTE, INCLUSIVE O ADUANEIRO"
+}, {
+dsProfissao: "DETETIVE PARTICULAR"
+}, {
+dsProfissao: "DIPLOMATA"
+}, {
+dsProfissao: "DISCOTECARIO, VIDEOTECARIO E AFINS"
+}, {
+dsProfissao: "DO LAR"
+}, {
+dsProfissao: "ECOLOGISTA"
+}, {
+dsProfissao: "ECONOMISTA"
+}, {
+dsProfissao: "ELETRICISTA E ASSEMELHADOS"
+}, {
+dsProfissao: "ELETRICITARIO"
+}, {
+dsProfissao: "EMPREGADO DOMESTICO"
+}, {
+dsProfissao: "EMPRESARIO"
+}, {
+dsProfissao: "EMPRESARIO E PRODUTOR DE ESPETACULOS PUBLICOS"
+}, {
+dsProfissao: "ENFERMEIRO"
+}, {
+dsProfissao: "ENGENHEIRO"
+}, {
+dsProfissao: "ESCREVENTE/AUXILIAR DE CARTORIO"
+}, {
+dsProfissao: "ESCRITOR"
+}, {
+dsProfissao: "ESCULTOR/PINTOR OU ASSEMELHADO"
+}, {
+dsProfissao: "ESPOLIO"
+}, {
+dsProfissao: "ESTAGIARIO E ASSEMELHADOS"
+}, {
+dsProfissao: "ESTATISTICO"
+}, {
+dsProfissao: "ESTETICISTA"
+}, {
+dsProfissao: "ESTIVADOR, CARREGADOR, EMBALADOR E ASSEMELHADOS"
+}, {
+dsProfissao: "ESTUDANTE"
+}, {
+dsProfissao: "FARMACEUTICO"
+}, {
+dsProfissao: "FAXINEIRO"
+}, {
+dsProfissao: "FEIRANTE"
+}, {
+dsProfissao: "FERRAMENTEIRO"
+}, {
+dsProfissao: "FERROVIARIO"
+}, {
+dsProfissao: "FILOLOGO"
+}, {
+dsProfissao: "FILOSOFO"
+}, {
+dsProfissao: "FISCAL, AUDITOR E ASSEMELHADOS"
+}, {
+dsProfissao: "FISICO"
+}, {
+dsProfissao: "FISIOTERAPEUTA OU TERAPEUTA OCUPACIONAL"
+}, {
+dsProfissao: "FLORICULTOR"
+}, {
+dsProfissao: "FONOAUDIOLOGO"
+}, {
+    cdProfissao: 1e3,
+    dsProfissao: "FOTOGRAFO"
+}, {
+dsProfissao: "FRENTISTA"
+}, {
+dsProfissao: "FUNILEIRO"
+}, {
+dsProfissao: "GARI OU VARREDOR DE LOGRADOUROS PUBLICOS"
+}, {
+dsProfissao: "GARIMPEIRO"
+}, {
+dsProfissao: "GEOGRAFO"
+}, {
+dsProfissao: "GEOLOGO"
+}, {
+dsProfissao: "GOVERNADOR DE ESTADO/DISTRITO FEDERAL"
+}, {
+dsProfissao: "HISTORIADOR"
+}, {
+dsProfissao: "INDUSTRIARIO"
+}, {
+dsProfissao: "INSTRUTOR"
+}, {
+dsProfissao: "JARDINEIRO"
+}, {
+dsProfissao: "JOALHEIRO E OURIVES"
+}, {
+dsProfissao: "JORNALEIRO"
+}, {
+dsProfissao: "JORNALISTA"
+}, {
+dsProfissao: "LANTERNEIRO E PINTOR DE VEICULOS"
+}, {
+dsProfissao: "LEILOEIRO, AVALIADOR E ASSEMELHADOS"
+}, {
+dsProfissao: "LOCUTOR E COMENTARISTA DE RADIO E TELEVISAO E RADIALISTA"
+}, {
+dsProfissao: "MAGISTRADO"
+}, {
+dsProfissao: "MAQUINISTA E FOGUISTA DE EMBARCACOES,LOCOMOTIVAS E ASSEMELHA"
+}, {
+dsProfissao: "MARCENEIRO E TRAB.ARTEFATOS MADEIRA"
+}, {
+dsProfissao: "MARINHEIRO"
+}, {
+dsProfissao: "MASSAGISTA"
+}, {
+dsProfissao: "MATEMATICO E ATUARIO"
+}, {
+dsProfissao: "MECANICO DE MANUTENCAO DE MAQUINAS INDUSTRIAIS"
+}, {
+dsProfissao: "MECANICO DE MANUTENCAO DE VEICULOS AUTOMOTORES"
+}, {
+dsProfissao: "MEDICO"
+}, {
+dsProfissao: "MENOR SEM ATIVIDADE"
+}, {
+dsProfissao: "MERGULHADOR"
+}, {
+dsProfissao: "MESTRE E CONTRAMESTRE"
+}, {
+dsProfissao: "METALURGICO E SIDERURGICO"
+}, {
+dsProfissao: "METEOROLOGISTA"
+}, {
+dsProfissao: "MILITAR DAS FORCAS ARMADAS - AERONAUTICA"
+}, {
+dsProfissao: "MILITAR DAS FORCAS ARMADAS - EXERCITO"
+}, {
+dsProfissao: "MILITAR DAS FORCAS ARMADAS - MARINHA"
+}, {
+dsProfissao: "MILITAR EM GERAL"
+}, {
+dsProfissao: "MINISTRO DE ESTADO"
+}, {
+dsProfissao: "MISSIONARIO"
+}, {
+dsProfissao: "MODELISTA E/OU CORTADOR - VESTUARIO"
+}, {
+dsProfissao: "MODELO DE MODAS"
+}, {
+dsProfissao: "MODISTA"
+}, {
+dsProfissao: "MOTOCICLISTA (TRANSP. DE MERCADORIAS) E/OU ASSEMELHADOS"
+}, {
+dsProfissao: "MOTORISTA"
+}, {
+dsProfissao: "MOTORISTA DE VEICULOS DE TRANSPORTE DE CARGA"
+}, {
+dsProfissao: "MOTORISTA DE VEICULOS DE TRANSPORTE DE PASSAGEIROS"
+}, {
+dsProfissao: "MOTORISTA/OPERADOR DE MAQUINAS PESADAS"
+}, {
+dsProfissao: "MUSICO"
+}, {
+dsProfissao: "NUTRICIONISTA"
+}, {
+dsProfissao: "OCEANOGRAFO"
+}, {
+dsProfissao: "ODONTOLOGO"
+}, {
+dsProfissao: "OFICIAL DAS FORCAS ARMADAS E FORCAS AUXILIARES"
+}, {
+dsProfissao: "OFICIAL DE JUSTICA"
+}, {
+dsProfissao: "OLEIRO"
+}, {
+dsProfissao: "OPERADOR DE COMPUTADORES"
+}, {
+dsProfissao: "OPERADOR DE MAQUINAS AGROPECUARIAS"
+}, {
+dsProfissao: "OPERADOR DE TELEMARKETING"
+}, {
+dsProfissao: "OPTICO"
+}, {
+dsProfissao: "ORNITOLOGO"
+}, {
+dsProfissao: "PADEIRO E CONFEITEIRO"
+}, {
+dsProfissao: "PASTOR"
+}, {
+dsProfissao: "PECUARISTA"
+}, {
+dsProfissao: "PEDAGOGO"
+}, {
+dsProfissao: "PESCADOR"
+}, {
+dsProfissao: "PESCADOR COM FINS COMERCIAIS"
+}, {
+dsProfissao: "PESQ/PRODUC DE MUDAS OU SEMENT FISCALIZADAS/CERTIFICADAS"
+}, {
+dsProfissao: "PESQ/PRODUC DE SEMEM PARA INSEMINACAO ARTIFICIAL"
+}, {
+dsProfissao: "PILOTO DE AERONAVES"
+}, {
+dsProfissao: "POLICIAL CIVIL"
+}, {
+dsProfissao: "PORTEIRO DE EDIFICIO, ASCENSORISTA E GARAGISTA"
+}, {
+dsProfissao: "PORTUARIO"
+}, {
+dsProfissao: "PREFEITO MUNICIPAL"
+}, {
+dsProfissao: "PRESIDENTE DA REPUBLICA"
+}, {
+dsProfissao: "PREST SERV DE INSEMINACAO ARTIFICIAL, EM IMOVEL RURAL"
+}, {
+dsProfissao: "PREST SERV MECANIZAD,NATURAL,RURAL INCLUSIVE PROTECAO SOLO"
+}, {
+dsProfissao: "PROCURADOR DA JUSTICA E ASSEMELHADOS"
+}, {
+dsProfissao: "PROFESSOR DE ENSINO FUNDAMENTAL E MEDIO"
+}, {
+dsProfissao: "PROFESSOR DE ENSINO SUPERIOR"
+}, {
+dsProfissao: "PROFESSOR/DEMAIS"
+}, {
+dsProfissao: "PROFISSIONAL DE LETRAS E ARTES"
+}, {
+dsProfissao: "PROGRAMADOR DE SISTEMA"
+}, {
+dsProfissao: "PROMOTOR DE JUSTICA"
+}, {
+dsProfissao: "PROMOTOR DE VENDAS E DEMONSTRADOR"
+}, {
+dsProfissao: "PROTETICO"
+}, {
+dsProfissao: "PSICOLOGO E PSICANALISTA"
+}, {
+dsProfissao: "PUBLICITARIO"
+}, {
+dsProfissao: "QUIMICO"
+}, {
+dsProfissao: "RADIOTECNICO"
+}, {
+dsProfissao: "REDATOR"
+}, {
+dsProfissao: "RELACOES PUBLICAS"
+}, {
+dsProfissao: "RELOJOEIRO"
+}, {
+dsProfissao: "RENTISTA OU LOCADOR"
+}, {
+dsProfissao: "REPRESENTANTE COMERCIAL"
+}, {
+dsProfissao: "RODOVIARIO"
+}, {
+dsProfissao: "SACERDOTE"
+}, {
+dsProfissao: "SALVA-VIDAS OU BOMBEIRO PARTICULAR/AUTONOMO"
+}, {
+dsProfissao: "SAPATEIRO E DEMAIS TRAB DE CALCADOS E ARTEFATOS DE COURO"
+}, {
+dsProfissao: "SECRETARIO ESTADUAL"
+}, {
+dsProfissao: "SECRETARIO FEDERAL"
+}, {
+dsProfissao: "SECRETARIO MUNICIPAL"
+}, {
+dsProfissao: "SECRETARIO/ESTENOGRAFO/DATIL/RECEP/TELEF TRAB ASSEMELHADOS"
+}, {
+dsProfissao: "SECURITARIO"
+}, {
+dsProfissao: "SEGURANCA/VIGILANTE"
+}, {
+dsProfissao: "SENADOR DA REPUBLICA"
+}, {
+dsProfissao: "SERRALHEIRO"
+}, {
+dsProfissao: "SERVENTUARIO DE JUSTICA"
+}, {
+dsProfissao: "SERVIDOR PUBLICO ESTADUAL"
+}, {
+dsProfissao: "SERVIDOR PUBLICO FEDERAL"
+}, {
+dsProfissao: "SERVIDOR PUBLICO MUNICIPAL"
+}, {
+dsProfissao: "SOCIOLOGO"
+}, {
+dsProfissao: "SUCATEIRO"
+}, {
+dsProfissao: "SUPERVISOR, INSPETOR E AGENTE DE COMPRAS E VENDAS"
+}, {
+dsProfissao: "TABELIAO/OFICIAL DE REGISTRO"
+}, {
+dsProfissao: "TAPECEIRO"
+}, {
+dsProfissao: "TAQUIGRAFO"
+}, {
+dsProfissao: "TAXISTA"
+}, {
+dsProfissao: "TECNICO AGRICOLA"
+}, {
+dsProfissao: "TECNICO DE BIOLOGIA"
+}, {
+dsProfissao: "TECNICO DE CONTABILIDADE E DE ESTATISTICA"
+}, {
+dsProfissao: "TECNICO DE EDIFICACOES"
+}, {
+dsProfissao: "TECNICO DE ELETRICIDADE, ELETRONICA E TELECOMUNICACOES"
+}, {
+dsProfissao: "TECNICO DE ENFERMAGEM"
+}, {
+dsProfissao: "TECNICO DE INFORMATICA"
+}, {
+dsProfissao: "TECNICO DE LABORATORIO E RAIOS X"
+}, {
+dsProfissao: "TECNICO DE MECANICA"
+}, {
+dsProfissao: "TECNICO DE QUIMICA"
+}, {
+dsProfissao: "TECNICO EM AUDIO E CENOGRAFIA"
+}, {
+dsProfissao: "TECNICO EM SEGURANCA DO TRABALHO"
+}, {
+dsProfissao: "TECNICO INSTRUMENTISTA"
+}, {
+dsProfissao: "TECNOLOGO"
+}, {
+dsProfissao: "TINTUREIRO"
+}, {
+dsProfissao: "TIPOGRAFO E DEMAIS TRABALHADORES DE ARTES GRAFICAS"
+}, {
+dsProfissao: "TOPOGRAFO"
+}, {
+dsProfissao: "TORNEIRO MECANICO"
+}, {
+dsProfissao: "TRAB DE SERVICOS DE CONTABILIDADE, CAIXA E ASSEMELHADOS"
+}, {
+dsProfissao: "TRABALHADOR DE CONSTRUCAO CIVIL"
+}, {
+dsProfissao: "TRABALHADOR DE EXTRACAO DE MINERIOS"
+}, {
+dsProfissao: "TRABALHADOR DE EXTRACAO VEGETAL"
+}, {
+dsProfissao: "TRABALHADOR DE FABRICACAO DE PAPEL E PAPELAO"
+}, {
+dsProfissao: "TRABALHADOR DE FABRICACAO DE PRODUTOS DE BORRACHA E PLASTICO"
+}, {
+dsProfissao: "TRABALHADOR DE FABRICACAO DE ROUPAS"
+}, {
+dsProfissao: "TRABALHADOR DE FABRICACAO E PREPARACAO DE ALIMENTOS E BEBIDA"
+}, {
+dsProfissao: "TRABALHADOR DE INSTALACOES DE PROCESSAMENTO QUIMICO"
+}, {
+dsProfissao: "TRABALHADOR DE USINAGEM DE METAIS"
+}, {
+dsProfissao: "TRABALHADOR FLORESTAL"
+}, {
+dsProfissao: "TRABALHADOR INFORMAL"
+}, {
+dsProfissao: "TRABALHADOR RURAL"
+}, {
+dsProfissao: "TRADUTOR"
+}, {
+dsProfissao: "VENDEDOR DE COMERCIO VAREJISTA E ATACADISTA"
+}, {
+dsProfissao: "VENDEDOR PRACISTA E CAIXEIRO VIAJANTE"
+}, {
+dsProfissao: "VEREADOR"
+}, {
+dsProfissao: "VETERINARIO OU ZOOTECNISTA"
+}, {
+dsProfissao: "VICE-GOVERNADOR DE ESTADO/DISTRITO FEDERAL"
+}, {
+dsProfissao: "VICE-PREFEITO MUNICIPAL"
+}, {
+dsProfissao: "VICE-PRESIDENTE DA REPUBLICA"
+}, {
+dsProfissao: "VIDRACEIRO"
+}, {
+dsProfissao: "ZELADOR DE EDIFICIO"
+}]
+
+document.addEventListener('DOMContentLoaded', function () {
+    const profissaoAtual = document.getElementById("profissaoAtual").value;
+    const select = document.getElementById("profissaoSelect");
+
+    profissoes.forEach(p => {
+        const option = document.createElement("option");
+        option.value = p.dsProfissao;
+        option.textContent = p.dsProfissao;
+
+        if (p.dsProfissao === profissaoAtual) {
+            option.selected = true;
+        }
+
+        select.appendChild(option);
+    });
+});
